67b536d2300d2c8b2907cac2593a45323df09f75
JinseonYe <aatwe24@gmail.com>
2023-01-19 16:14:45 +0900

    [Refactor] ProductController 시큐리티 적용

--- src/main/java/com/dbzz/matchingproject/controller/ProductController.java
+++ src/main/java/com/dbzz/matchingproject/controller/ProductController.java
@@ -5,9 +5,7 @@ import com.dbzz.matchingproject.dto.request.UpdateProductRequestDto;
 import com.dbzz.matchingproject.dto.response.ProductResponseDto;
 import com.dbzz.matchingproject.dto.response.StatusResponseDto;
 import com.dbzz.matchingproject.enums.StatusEnum;
-import com.dbzz.matchingproject.jwt.JwtUtil;
 import com.dbzz.matchingproject.service.ProductService;
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,50 +18,39 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ProductController {
     private final ProductService productService;
-    private final JwtUtil jwtUtil;
 
     //판매상품 등록
-    @PostMapping("/products/{userId}")
-    public ResponseEntity<StatusResponseDto> createProductPage(@PathVariable String userId, @RequestBody CreateProductRequestDto requestDto, HttpServletRequest request) {
+    @PostMapping("/products")
+    public ResponseEntity<StatusResponseDto> createProductPage(@PathVariable String userId, @RequestBody CreateProductRequestDto requestDto) {
         StatusResponseDto responseDto = new StatusResponseDto(StatusEnum.OK, "상품 등록 완료");
-        String token = jwtUtil.resolveToken(request);
-        jwtUtil.validateAndGetUserInfo(token);
         productService.createProductPage(userId, requestDto);
         return new ResponseEntity<>(responseDto, HttpStatus.OK);
     }
 
     //나의 판매상품 조회
-    @GetMapping("/products/{userId}")
-    public List<ProductResponseDto> getAllProductByUserId(@PathVariable String userId, HttpServletRequest request) {
-        String token = jwtUtil.resolveToken(request);
-        jwtUtil.validateAndGetUserInfo(token);
+    @GetMapping("/allproducts")
+    public List<ProductResponseDto> getAllProductByUserId(@PathVariable String userId) {
         return productService.getAllProductByUserId(userId);
     }
 
     //전체 상품 조회(고객용)
     @GetMapping("/products")
-    public List<ProductResponseDto> getAllProducts(HttpServletRequest request) {
-        String token = jwtUtil.resolveToken(request);
-        jwtUtil.validateAndGetUserInfo(token);
+    public List<ProductResponseDto> getAllProducts() {
         return productService.getAllProducts();
     }
 
     //판매상품 수정
     @PutMapping("/products/{productId}")
-    public ResponseEntity<StatusResponseDto> updateProduct(@PathVariable Long productId, @RequestBody UpdateProductRequestDto requestDto, HttpServletRequest request) {
+    public ResponseEntity<StatusResponseDto> updateProduct(@PathVariable Long productId, @RequestBody UpdateProductRequestDto requestDto) {
         StatusResponseDto responseDto = new StatusResponseDto(StatusEnum.OK, "상품 수정 완료");
-        String token = jwtUtil.resolveToken(request);
-        jwtUtil.validateAndGetUserInfo(token);
         productService.updateProduct(productId, requestDto);
         return new ResponseEntity<>(responseDto, HttpStatus.OK);
     }
 
     //판매상품 삭제
     @DeleteMapping("/products/{productId}")
-    public ResponseEntity<StatusResponseDto> deleteProduct(@PathVariable Long productId, HttpServletRequest request) {
+    public ResponseEntity<StatusResponseDto> deleteProduct(@PathVariable Long productId) {
         StatusResponseDto responseDto = new StatusResponseDto(StatusEnum.OK, "상품 삭제 완료");
-        String token = jwtUtil.resolveToken(request);
-        jwtUtil.validateAndGetUserInfo(token);
         productService.deleteProduct(productId);
         return new ResponseEntity<>(responseDto, HttpStatus.OK);
     }
