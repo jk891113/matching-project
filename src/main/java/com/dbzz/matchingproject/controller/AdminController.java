938133c069069c6e1ad3a82f6c292c6485df5bcb
elicho <elicho91@kakao.com>
2023-01-19 19:20:33 +0900

    [Feat] 어드민 시큐리티 적용 완료

--- src/main/java/com/dbzz/matchingproject/controller/AdminController.java
+++ src/main/java/com/dbzz/matchingproject/controller/AdminController.java
@@ -5,58 +5,47 @@ import com.dbzz.matchingproject.dto.response.SellerListResponseDto;
 import com.dbzz.matchingproject.dto.response.StatusResponseDto;
 import com.dbzz.matchingproject.dto.response.UserResponseDto;
 import com.dbzz.matchingproject.enums.StatusEnum;
-import com.dbzz.matchingproject.jwt.JwtUtil;
 import com.dbzz.matchingproject.service.AdminService;
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@Secured({"ROLE_ADMIN"})
 public class AdminController {
 
     private final AdminService adminService;
-    private final JwtUtil jwtUtil;
 
     @GetMapping("/admin/customer-list")
-    public List<UserResponseDto> getAllCustomers(HttpServletRequest request) {
-        String token = jwtUtil.resolveToken(request);
-        jwtUtil.validateAndGetUserInfo(token);
+    public List<UserResponseDto> getAllCustomers() {
         return adminService.getAllCustomers();
     }
 
     @GetMapping("/admin/seller-list")
-    public List<SellerListResponseDto> getAllSellers(HttpServletRequest request) {
-        String token = jwtUtil.resolveToken(request);
-        jwtUtil.validateAndGetUserInfo(token);
+    public List<SellerListResponseDto> getAllSellers() {
         return adminService.getAllSellers();
     }
 
     @GetMapping("/admin/permission")
-    public List<PermissionResponseDto> getPermissionRequestForms(HttpServletRequest request){
-        String token = jwtUtil.resolveToken(request);
-        jwtUtil.validateAndGetUserInfo(token);
+    public List<PermissionResponseDto> getPermissionRequestForms(){
         return adminService.getPermissionRequestForms();
     }
 
     @PutMapping("/admin/users/{userId}/permission")
-    public ResponseEntity<StatusResponseDto> permitAuth(@PathVariable String userId, HttpServletRequest request){
+    public ResponseEntity<StatusResponseDto> permitAuth(@PathVariable String userId){
         StatusResponseDto responseDto = new StatusResponseDto(StatusEnum.OK, "판매자 권한 승인 완료");
-        String token = jwtUtil.resolveToken(request);
-        jwtUtil.validateAndGetUserInfo(token);
         adminService.permitAuth(userId);
         return new ResponseEntity<>(responseDto, HttpStatus.OK);
     }
 
     @PutMapping("/admin/sellers/{userId}/permission")
-    public ResponseEntity<StatusResponseDto> removeAuth(@PathVariable String userId, HttpServletRequest request){
+    public ResponseEntity<StatusResponseDto> removeAuth(@PathVariable String userId){
         StatusResponseDto responseDto = new StatusResponseDto(StatusEnum.OK, "판매자 권한 회수 완료");
-        String token = jwtUtil.resolveToken(request);
-        jwtUtil.validateAndGetUserInfo(token);
         adminService.removeAuth(userId);
         return new ResponseEntity<>(responseDto, HttpStatus.OK);
     }
